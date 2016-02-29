0aff881b38614d3627d7c566356dd7578a8fe57a
Jin-Hwan CHO <chof@nims.re.kr>
2016-02-29 22:18:09 +0900

    brew audit --strict --online chomp

--- chomp.rb
+++ chomp.rb
@@ -2,7 +2,7 @@ class Chomp < Formula
   desc "Computational Homology Project software"
   homepage "https://github.com/shaunharker/CHomP"
   url "https://github.com/shaunharker/CHomP/archive/v1.2.tar.gz"
-  #version "1.2"
+  # version "1.2"
   sha256 "44018462fea4c9354c62ab4b580b5cca01ecb1a0977070c190a2b353ea734fac"
 
   depends_on "cmake" => :build
@@ -14,7 +14,7 @@ class Chomp < Formula
     # Hack to work with Xquartz installed on /opt/X11/
     inreplace "CMakeLists.txt", "/opt/local", "/opt/X11"
 
-    system ".install/build.sh --prefix=/usr/local/Cellar/chomp/1.2"
+    system ".install/build.sh", "--prefix=/usr/local/Cellar/chomp/1.2"
   end
 
   test do
